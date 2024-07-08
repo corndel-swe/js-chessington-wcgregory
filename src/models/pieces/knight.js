9d42b1564e101e3354ab4cc6e781d9849e481e32
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-08 16:28:38 +0000

    refactor of the knight piece moves

--- src/models/pieces/knight.js
+++ src/models/pieces/knight.js
@@ -17,36 +17,28 @@ export default class Knight extends Piece {
     const moves = Array()
 
     if (location.row + 2 <= 7 && location.col + 1 <= 7) {  // can move "north-east" as a knight up 2 right 1
-      let knightmove = new Square(location.row + 2, location.col + 1)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row + 2, location.col + 1))
     }
     if (location.row + 1 <= 7 && location.col + 2 <= 7) {  // move "north-east" as a knight up 1 right 2
-      let knightmove = new Square(location.row + 1, location.col + 2)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row + 1, location.col + 2))
     }
     if (location.row - 2 >= 0 && location.col + 1 <= 7) {  // can move "south-east" as a knight down 2 right 1
-      let knightmove = new Square(location.row - 2, location.col + 1)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row - 2, location.col + 1))
     }
     if (location.row - 1 >= 0 && location.col + 2 <= 7) {  // can move "south-east" as a knight down 1 right 2
-      let knightmove = new Square(location.row - 1, location.col + 2)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row - 1, location.col + 2))
     }
     if (location.row - 2 >= 0 && location.col - 1 >= 0) {  // can move "south-west" as a knight down 2 left 1
-      let knightmove = new Square(location.row - 2, location.col - 1)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row - 2, location.col - 1))
     }
     if (location.row - 1 >= 0 && location.col - 2 >= 0) {  // can move "south-west" as a knight down 1 left 2
-      let knightmove = new Square(location.row - 1, location.col - 2)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row - 1, location.col - 2))
     }
     if (location.row + 2 <= 7 && location.col - 1 >= 0) {  // can move "north-west" as a knight up 2 left 1
-      let knightmove = new Square(location.row + 2, location.col - 1)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row + 2, location.col - 1))
     }
     if (location.row + 1 <= 7 && location.col - 2 >= 0) {  // can move "north-west" as a knight up 1 left 2
-      let knightmove = new Square(location.row + 1, location.col - 2)
-      possibleKnightMoves.push(knightmove)
+      possibleKnightMoves.push(new Square(location.row + 1, location.col - 2))
     }
 
     for (let move of possibleKnightMoves) {
